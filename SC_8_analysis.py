3c82ba770ff6f07327b0fca3c720a6ad22fbbec0
David Underdown <david.underdown@nationalarchives.gsi.gov.uk>
2018-03-05 22:47:01 +0000

    Fix plotting,get individual outputs now, and decent layout

--- SC_8_analysis.py
+++ SC_8_analysis.py
@@ -1,5 +1,7 @@
 import pandas as pd;
 import matplotlib.pyplot as plt;
+import matplotlib.tight_layout as lyt;
+import matplotlib;
 
 df1=pd.read_csv(r"..\DiscoveryAPI\myRecords.csv",parse_dates=["numStartDate","numEndDate"],infer_datetime_format=True,dayfirst=True)
 counties=["Yorkshire","Kent","Norfolk","Hampshire","Devon","Northumberland","Cornwall","Lincolnshire","Surrey","Oxfordshire","Suffolk","Sussex","Berkshire","Essex","Gloucestershire","Warwickshire","Wiltshire","Buckinghamshire","Dorset","Somerset","Cambridgeshire","Middlesex","Northamptonshire","Hertfordshire","Shropshire","Staffordshire","Nottinghamshire","Lancashire","Cheshire","Derbyshire","Cumberland","Huntingdonshire","Leicestershire","Channel Islands","Denbighshire","County Durham","Herefordshire","Flintshire","Bedfordshire","Westmorland","Worcestershire","Monmouthshire","Rutland","Caernarfonshire","Pembrokeshire","Brecknockshire","Cardiganshire","Radnorshire","Merionethshire","Anglesey","Glamorganshire","Carmarthenshire","Montgomeryshire"]
@@ -42,12 +44,27 @@ df3.to_csv("SC_8_summed_by_county.csv")
 df4.to_csv("SC_8_summed_by_county_before_1370.csv")
 df5.to_csv("SC_8_summed_by_county_after_1370.csv")
 
-plot1=df3.plot.bar()
-fig1=plot1.get_figure()
-fig1.savefig("SC_8_summed_by_county.png")
-plot2=df4.plot.bar()
-fig2=plot2.get_figure()
+
+fig1, ax1 = plt.subplots()
+axSubPlot1=df3.plot.bar(ax=ax1,color="xkcd:royal blue")#,figsize(15,14)
+ax1.set_xlabel("Counties")
+ax1.set_ylabel("Number of petitions")
+ax1.yaxis.set_minor_locator(matplotlib.ticker.AutoMinorLocator(n=10))
+fig1.tight_layout()
+fig1.savefig("SC_8_summed_by_county.png",bbox='tight',pad_inches=5)
+
+fig2, ax2 = plt.subplots()
+axSubPlot2=df4.plot.bar(ax=ax2,color="xkcd:red")#,figsize=(9,14)
+ax2.set_xlabel("Counties")
+ax2.set_ylabel("Number of petitions")
+ax2.yaxis.set_minor_locator(matplotlib.ticker.AutoMinorLocator(n=10))
+fig2.tight_layout()
 fig2.savefig("SC_8_summed_by_county_before_1370.png")
-plot3=df5.plot.bar()
-fig3=plot3.get_figure()
+
+fig3, ax3 = plt.subplots()
+axSubPlot3=df5.plot.bar(ax=ax3,color="xkcd:yellow")#,figsize=(9,14)
+ax3.set_xlabel("Counties")
+ax3.set_ylabel("Number of petitions")
+ax3.yaxis.set_minor_locator(matplotlib.ticker.AutoMinorLocator(n=10))
+fig3.tight_layout()
 fig3.savefig("SC_8_summed_by_county_after_1370.png")
